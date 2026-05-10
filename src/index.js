c0c5f934c1e61cf585f7e6e9c89a7936eef3b514
harshit <harshitkamboj404@gmail.com>
2026-05-10 15:27:59 +0530

    Deployment config

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const taskRoutes = require("./routes/taskRoutes");
 app.use("/api/tasks", taskRoutes);
 
